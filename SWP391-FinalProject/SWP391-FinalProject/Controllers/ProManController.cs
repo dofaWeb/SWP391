5ee055cda98a6a87a84176b45c7f0dd5fe35ea41
SOLPTT <0858085036tan@gmail.com>
2024-10-02 10:58:08 +0700

    tan first commit

---
+++ SWP391-FinalProject/SWP391-FinalProject/Controllers/ProManController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391_FinalProject.Controllers
+{
+    public class ProManController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

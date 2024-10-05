7e39b2a0502ed5be0db8b3a9de980f9a37c593e8
Anh Nguyen <gioakim0432004@gmail.com>
2024-10-05 09:50:29 +0700

    Set up layout for product and login

--- SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
+++ SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3af4ad15160169d264dc4ef5973e5921e24ed99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1668ed800a4854d9644ffe9b7116e841c1b88e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

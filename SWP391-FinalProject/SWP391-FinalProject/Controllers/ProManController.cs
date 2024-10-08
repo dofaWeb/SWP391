6b11babde1a3d5a201c57cd5e9c50336b3467c48
dofaWeb <gioakim0432004@gmail.com>
2024-10-08 20:00:16 +0700

    Fix display product in customer page and admin page

--- SWP391-FinalProject/SWP391-FinalProject/Controllers/ProManController.cs
+++ SWP391-FinalProject/SWP391-FinalProject/Controllers/ProManController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SWP391_FinalProject.Entities;
 using SWP391_FinalProject.Models;
+using SWP391_FinalProject.Repository;
 using System.Net.Http.Headers;
 
 namespace SWP391_FinalProject.Controllers
@@ -21,31 +22,27 @@ namespace SWP391_FinalProject.Controllers
 
         public IActionResult Display()
         {
-            var query = from p in db.Products
-                        join pi in db.ProductItems on p.Id equals pi.ProductId
-                        join b in db.Categories on p.CategoryId equals b.Id
-                        join ps in db.ProductStates on p.StateId equals ps.Id
-                        group pi by new { p.Id, p.Name, p.Picture, p.Description, Category = b.Name, State = ps.Name } into g
-                        select new Models.ProductModel
-                        {
-                            Id = g.Key.Id,
-                            Name = g.Key.Name,
-                            Picture = g.Key.Picture,
-                            Description = g.Key.Description,
-                            CategoryId = g.Key.Category,
-                            State = g.Key.State,
-                            Quantity = g.Sum(pi => pi.Quantity)
-                        };
+            Repository.Product proRepo = new Repository.Product(db);
+            var query = proRepo.GetAllProduct();
 
             return View(query);
         }
         [HttpGet]
         public IActionResult AddProduct()
         {
+            Repository.Product proRepo = new Repository.Product(db);
+            Repository.Category catRepo = new Repository.Category(db);
+
+            ViewBag.NewProductId = proRepo.getNewProductID();
+            ViewBag.Category = catRepo.GetAllCategory();
+            ViewBag.ProductStates = proRepo.getAllProductState();
             return View();
         }
 
         [HttpPost]
-        public IActionResult AddProduct(object model) => RedirectToAction("Display");
+        public IActionResult AddProduct(Models.ProductModel model)
+        {
+            return RedirectToAction("Display");
+        }
     }
 }

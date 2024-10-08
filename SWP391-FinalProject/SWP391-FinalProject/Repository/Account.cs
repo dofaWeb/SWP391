d2149dfe519855522a8c78f32a68016dc88b46bd
SOLPTT <0858085036tan@gmail.com>
2024-10-08 09:15:11 +0700

    register

---
+++ SWP391-FinalProject/SWP391-FinalProject/Repository/Account.cs
@@ -0,0 +1,36 @@
+﻿using SWP391_FinalProject.Entities;
+
+namespace SWP391_FinalProject.Repository
+{
+    public class Account
+    {
+        private readonly DBContext db;
+
+        public Account(DBContext context)
+        {
+            db = context;
+        }
+        public string GetNewId()
+        {
+            string lastId = db.Accounts
+                     .OrderByDescending(a => a.Id)
+                     .Select(a => a.Id)
+                     .FirstOrDefault();
+            if (lastId == null)
+            {
+                return "A0000001";
+            }
+            // Tách phần chữ (A) và phần số (0000001)
+            string prefix = lastId.Substring(0, 1); // Lấy ký tự đầu tiên
+            int number = int.Parse(lastId.Substring(1)); // Lấy phần số và chuyển thành số nguyên
+
+            // Tăng số lên 1
+            int newNumber = number + 1;
+
+            // Tạo ID mới với số đã tăng, định dạng lại với 7 chữ số
+            string newId = $"{prefix}{newNumber:D7}";
+
+            return newId;
+        }
+    }
+}

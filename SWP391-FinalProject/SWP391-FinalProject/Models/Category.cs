c06460582de1759eea6151778883c31bd1b2acb3
dofaWeb <gioakim0432004@gmail.com>
2024-10-08 10:40:58 +0700

    Set up category

---
+++ SWP391-FinalProject/SWP391-FinalProject/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace SWP391_FinalProject.Models
+{
+    public class Category
+    {
+        public String Name {  get; set; }
+    }
+}
---
+++ SWP391-FinalProject/SWP391-FinalProject/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SWP391_FinalProject.Entities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SWP391_FinalProject.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly DBContext db;
+
+        public CategoryViewComponent(DBContext context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = db.Categories.Select(l => new Category
+            {
+                Name = l.Name,
+            });
+            return View(data);
+        }
+    }
+}

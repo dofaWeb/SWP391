352371d9fbb9986bb253fc8bf7b561de284f2479
dofaWeb <gioakim0432004@gmail.com>
2024-10-09 14:55:20 +0700

    Modify register system

--- SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
+++ SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d934af4bffee7d3b233ea6e645f41c7df419cfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889416f4504d2eae9513cdb2ef7dd1d2a46909a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

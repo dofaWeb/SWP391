f6aed23d94a558aeb1126deb757b75355759e866
dofaWeb <gioakim0432004@gmail.com>
2024-10-09 14:41:17 +0700

    Modify register system

--- SWP391-FinalProject/SWP391-FinalProject/Controllers/AccController.cs
+++ SWP391-FinalProject/SWP391-FinalProject/Controllers/AccController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SWP391_FinalProject.Entities;
+using SWP391_FinalProject.Helpers;
 using SWP391_FinalProject.Models;
 using SWP391_FinalProject.Repository;
+using System.Security.Claims;
 
 namespace SWP391_FinalProject.Controllers
 {
@@ -14,6 +17,7 @@ namespace SWP391_FinalProject.Controllers
         {
             db= context;
         }
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
@@ -31,9 +35,39 @@ namespace SWP391_FinalProject.Controllers
         [HttpPost]
         public IActionResult Register(Models.AccountModel model)
         {
-            Repository.Account accRepo = new Repository.Account(db); 
-            accRepo.AddAccount(model);
-            return RedirectToAction("Index", "Pro");
+            Repository.Account accRepo = new Repository.Account(db);
+            if (accRepo.CheckEmail(model.Email))
+            {
+                accRepo.AddAccount(model);
+                return RedirectToAction("Index", "Pro");
+            }
+            else
+            {
+                ViewBag.Error = "Email has been used";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Models.AccountModel model)
+        {
+            Repository.Account accRepo = new Repository.Account(db);
+            if (accRepo.Login(model.Username, model.Password))
+            {
+                var user = accRepo.GetUserByUsername(model.Username);
+                var claims = new List<Claim> {
+                                new Claim(ClaimTypes.Email, user.Email),
+                                new Claim(ClaimTypes.Name, user.Name),
+                                new Claim(MySetting.CLAIM_CUSTOMERID, user.Id),
+                                new Claim(ClaimTypes.Role, user.RoleId)
+                            };
+                var claimsIdentity = new ClaimsIdentity(claims, "login");
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                await HttpContext.SignInAsync(claimsPrincipal);
+                return RedirectToAction("Index", "Pro");
+            }
+            return View();
         }
     }
 }
--- SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
+++ SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3ce08f217089f1d9bd876c5819c41e5ec7fc17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d934af4bffee7d3b233ea6e645f41c7df419cfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

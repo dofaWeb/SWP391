a6d871606910c1d95cb6040c1436fdee980dbd6d
wibuLord <giaohieu221203@gmail.com>
2024-10-10 11:20:04 +0700

    DisPlay_Profile

--- SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
+++ SWP391-FinalProject/SWP391-FinalProject/obj/Debug/net8.0/SWP391-FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b6754660d9da42d2499ac3d5969dc437a69570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94bb22a7063853775ba61b5f34176147e6cf4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391-FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
